d9b12c156ba08aa4a5eac1d938e1158656f4c745
SurabhiDevadiga <surabhidevadiga16@gmail.com>
2023-12-04 18:40:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,240 @@
+// console.log("hello world");
+
+//  var num =10;
+//  console.log(num);
+//  num="hello world";
+
+//  console.log(num)
+
+
+// let happy="hello world";
+// happy=10;
+
+
+// console.log(happy);
+
+// const a="happy";
+// a=100;
+
+
+// console.log(a)
+
+
+// {
+//     var s="hello world"
+   
+// }
+// console.log(s)
+
+
+
+// var b;
+// console.log(b);
+
+
+// for(let i=0;i<5;i++){
+//     console.log(i)
+
+
+// const arr=[10,20,30,40]
+// console.log(arr.length); 
+
+// let arr=[20,50,60,80]
+// arr.push(100)
+
+// console.log(arr.length);
+// console.log(arr);
+
+// let arr=[20,50,60,80]
+// arr.push(100)
+// arr.pop();
+// console.log(arr);
+// arr.pop();
+
+// console.log(arr);
+
+
+// let arr=[20,50,60,80]
+// const s=arr.indexOf(60)
+// console.log(arr.indexOf(60));
+
+
+// let arr=[20,50,"hai",false,16.5];
+// console.log(arr[2]);
+
+// console.log(arr[4]);
+
+// let [a,b,c]=[20,50,"hai",false,16.5];
+// console.log(a);
+// console.log(b);
+
+
+// const [val,val2] =["aws","google","twitter","instagram","facebook","microsoft","digitalocean"]
+
+// console.log(val,val2);
+
+// const val =["aws","google","twitter","instagram","facebook","microsoft","digitalocean"]
+// console.log(...val)
+// console.log(val)
+
+
+// const [val,val2,val3] =[40,"google","twitter","instagram","facebook","microsoft","digitalocean"]
+// console.log(...val,...val2,...val3);
+
+// const [a,b,c] =["google","twitter","instagram","facebook","microsoft","digitalocean"];
+// const arr=[...c];
+// console.log(arr[0]);
+// console.log(arr);
+
+// const val =["aws","google","twitter","instagram","facebook","microsoft","digitalocean"];
+// const arr=[...val,"hai","hello"];
+// console.log(arr);
+
+// const val =["aws","google","twitter","instagram","facebook","microsoft","digitalocean"];
+//  const arr=["hai","hello"];
+//  for(let i=0;i<val.length;i++){
+//     arr.push(val[i]);
+//     arr.pop();
+//  }
+//  console.log(arr);
+
+
+// const obj ={
+//     name:"hai",
+//     branch:"ise",
+//     usn:"is062",
+//     hobbies:["music","chess","coding","painting",]
+
+// }
+// console.log(...obj.name,...obj.branch);
+// console.log(...obj.usn);
+// console.log(...obj.hobbies[0]);
+// console.log(...obj.name,...obj.branch,...obj.usn,...obj.hobbies[0]);
+
+
+
+//  const obj ={
+//           name:"hai",
+//          branch:"ise",
+//          usn:"is062",
+//         hobbies:["music","chess","coding","painting",]
+//         a:1
+//      }
+
+// const [a,b]=[10,60,50,40];
+// console.log(a,b);
+
+
+// const{a,d,c}=obj;
+// console.log(a,b,c);
+
+// const obj ={
+//         name:"hai",
+//         branch:"ise",
+//          usn:"is062",
+//         hobbies:["music","chess","coding","painting",]
+//         a:1
+    
+//      }
+// const
+
+// const  arr={a:10};
+// console.log(typeof arr);
+
+// const val=30;
+
+// console.log(typeof val);
+
+
+// var a;
+// console.log(typeof a);
+
+
+// const obj={
+
+//         name:"surabhi",
+//         usn:"060",
+//         key:16,
+
+// };
+// const key ="name";
+// var val="usn";
+// console.log(obj.key);
+// console.log(obj[key]);
+// console.log(obj.usn);
+// console.log(obj[val]);
+
+// const obj={
+
+//         name:"surabhi",
+//         usn:"060",
+//         key:16,
+
+// };
+// const keys =["name","usn","key"]
+// for(let i=0;i<keys.length;i++)
+// console.log(obj.name);
+// console.log(obj[keys[i]]);
+
+
+// const acc={
+//         userName:"smile",
+//         password:"smile@123",
+//         details:{email:["smile@gmail.com","smile1@gmail.com"],phno:1234567890}
+        
+// }
+
+// console.log(acc.details.email)
+// console.log(acc.details.email[0])
+// const [primaryEmail,recoveryEmail]=acc.details.email;
+// console.log(primaryEmail,recoveryEmail);
+// console.log(...primaryEmail);
+// const emails=acc.details.email;
+// console.log(...emails)
+
+
+
+
+
+// const details={
+
+//         userName:"surabhi",
+//         DOB:"16-11-2004",
+//         branch:"ise",
+//         sub:{programLang:["python","java","c++"],usn:60}
+
+
+// }
+// console.log(details.sub.programLang)
+// console.log(details.email)
+
+
+// const googleDatabase=[{gmail:"test@gmail.com",owner:"test",password:"1234"},{gmail:"abc@gmail",owner:"abc",password:"953"}]
+// const len =googleDatabase.length;
+// for(let i=0;i<len;i++){
+        // console.log(googleDatabase[i].gmail)  
+        // console.log(googleDatabase[i].password)
+        // console.log(googleDatabase[i].owner)
+        // console.log(googleDatabase[i].owner[0])
+        // console.log(googleDatabase[i].password[1])
+        // console.log(googleDatabase)
+        // // console.log(googleDatabase[i].owner.gmail)
+        // console.log(googleDatabase[i].owner,googleDatabase[i].password)
+
+
+// }
+
+
+// const a=[20,30,40];
+// const b =89;
+
+// console.log(`hey the array is ${a} and the number is${b}`)
+// const str =`hey the number is ${b} and array is ${a}`;
+// console.log(str);
+// const num =(`array is ${a[0]}`)
+//  console.log(num);
+
+
+// console.log(console);
+// console.error("hello");
+console.log(error);
